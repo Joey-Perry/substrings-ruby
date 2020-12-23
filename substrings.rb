bb796398eae8706e9fdbe46281472d43526d9c64
Joey-Perry <Joeyap84@gmail.com>
2020-12-22 19:28:18 -0600

    Finished substrings step 1: one word

--- substrings.rb
+++ substrings.rb
@@ -1,4 +1,21 @@
 def substrings(string, dictionary_array)
-    #dictionary = = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-    
+    p formatted_string = string.downcase.chars.select { |char| ("a".."z").include?(char)}.join()
+    result = {}
+    dictionary_array.each do |word|
+        if formatted_string.include?(word)
+            result[word] = formatted_string.count(word)
+        else
+            nil
+        end
+    end
+
+    p result
 end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+# substrings("below", dictionary)
+# => { "below" => 1, "low" => 1 }
+substrings("Howdy partner, sit down! How's it going?", dictionary)
+#   => { "down" => 1, "go" => 1, "going" => 1, "how" => 2, "howdy" => 1, "it" => 2, "i" => 3, "own" => 1, "part" => 1, 
+# "partner" => 1, "sit" => 1 }
