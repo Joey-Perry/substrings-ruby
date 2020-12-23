2d8022681b0d1b05879e041b9c241824cd7f7355
Joey-Perry <Joeyap84@gmail.com>
2020-12-23 07:49:30 -0600

    Finished substring assignment

--- substrings.rb
+++ substrings.rb
@@ -1,9 +1,9 @@
 def substrings(string, dictionary_array)
-    p formatted_string = string.downcase.chars.select { |char| ("a".."z").include?(char)}.join()
     result = {}
+    formatted_string = string.downcase
     dictionary_array.each do |word|
         if formatted_string.include?(word)
-            result[word] = formatted_string.count(word)
+            result[word] = formatted_string.scan(word).length
         else
             nil
         end
@@ -14,8 +14,11 @@ end
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
+# Test case # 1
 # substrings("below", dictionary)
 # => { "below" => 1, "low" => 1 }
-substrings("Howdy partner, sit down! How's it going?", dictionary)
-#   => { "down" => 1, "go" => 1, "going" => 1, "how" => 2, "howdy" => 1, "it" => 2, "i" => 3, "own" => 1, "part" => 1, 
+
+# Test case # 2
+#substrings("Howdy partner, sit down! How's it going?", dictionary)
+# => { "down" => 1, "go" => 1, "going" => 1, "how" => 2, "howdy" => 1, "it" => 2, "i" => 3, "own" => 1, "part" => 1, 
 # "partner" => 1, "sit" => 1 }
